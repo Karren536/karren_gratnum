ed6014e72dd493e9feb8fbdd35e1887ab3b93a52
karren <mutanukarren@gmail.com>
2026-03-23 15:51:23 +0300

    changes

--- src/components/AddProductComponent.jsx
+++ src/components/AddProductComponent.jsx
@@ -1,81 +1,129 @@
-import React,{useState} from "react"
-import axios from "axios";
+import { useState } from "react";
 import NavbarComponent from "./NavbarComponent";
-const AddProductComponent =()=> {
-    let [productName, setProductName] = useState('')
-    let [productCategory, setProductCategory] = useState('')
-    let [productCost, setProductCost] = useState('')
-    let [productImage, setProductImage] = useState(null)
-    let [loading, setLoading] = useState('')
-    let [error, setError] = useState('')
-    let [success, setSuccess] = useState('')
+import axios from "axios";
+
+const AddProductComponent = () => {
+    let [product_name, setProductName] = useState("");
+    let [product_description, setProductDescription] = useState("");
+    let [product_cost, setProductCost] = useState("");
+    let [product_category, setProductCategory] = useState("");
+    let [product_image, setProductImage] = useState("");
+
+    let [loading, setLoading] = useState("");
+    let [error, setError] = useState("");
+    let [success, setSuccess] = useState("");
+
     const handleSubmit = async (e) => {
-        e.preventDefault();
-        console.log(productName, productCategory, productCost)
+        e.preventDefault()
+
         setError("")
         setSuccess("")
-        setLoading("Adding product please wait...")
+        setLoading("Please wait ...")
 
         try {
-            const product_data = new FormData();
-            product_data.append("name", productName);
-            product_data.append("category", productCategory);
-            product_data.append("cost", productCost);
-            product_data.append("image", productImage);
+            const product_data = new FormData()
 
-            const response = await axios.post('https://karren.alwaysdata.net/api/add_product', product_data)
+            product_data.append("product_name", product_name)
+            product_data.append("product_cost", product_cost)
+            product_data.append("product_category", product_category)
+            product_data.append("product_description", product_description)
+            product_data.append("product_image", product_image)
+
+            const response = await axios.post("https://karren.alwaysdata.net/api/add_product", product_data)
             console.log(response)
-            setLoading('')
-            setSuccess('Product added successfully!')
+
+            if (response.status === 200) {
+                setLoading("")
+                setSuccess(response.data.message)
+                // clear the form
+                setProductCategory("")
+                setProductCost("")
+                setProductDescription("")
+                setProductImage("")            
+                setProductName("")
+            }
+
         } catch (error) {
-            console.log(error)
-            setLoading('')
-            setError(error.message);
+            setLoading("")
+            setSuccess("")
+            setError(error.message)
         }
     }
+
     return (
-        <div className="row justify-content-center mt-4">
+        <div className="row justify-content-center my-3">
             <NavbarComponent />
             <div className="col-md-6 card shadow p-4">
-                <h2>Add New Product</h2>
+                <h2>Add product</h2>
                 <h5 className="text-warning">{loading}</h5>
                 <h5 className="text-danger">{error}</h5>
                 <h5 className="text-success">{success}</h5>
                 <form onSubmit={handleSubmit}>
-                        <input type="text"
-                            className="form-control my-3" 
-                            placeholder="Enter Product Name" 
-                            onChange={(e)=>{setProductName(e.target.value)}} 
-                            required
-                            value={productName}
-                        />
-                        <select  
-                            className="form-select" 
-                            onChange={(e)=>{setProductCategory(e.target.value)}} 
-                            required>
-                            <option value="">Select Product Category</option>
-                            <option value="TVs">televisions</option>
-                            <option value="laptops">laptops</option>
-                            <option value="accessories ">Accessories</option>
-                            <option value="Camera&lenses">C&L</option>
-                        </select>
-                        <input type="number"
-                            placeholder="Enter Product Cost"
-                            className="form-control my-3"
-                            onChange={(e)=>{setProductCost(e.target.value)}}
-                            required
-                            value={productCost}
-                        />
-                        <input type="file" 
-                            className="form-control my-3" 
-                            onChange={(e)=>{setProductImage(e.target.files[0])}} 
-                            required 
-                        />
-                        <button type="submit" className="btn btn-primary">Add Product</button>
+                    <input
+                        type="text"
+                        className="form-control"
+                        required
+                        placeholder="Enter product name"
+                        value={product_name}
+                        onChange={(e) => {
+                            setProductName(e.target.value);
+                        }}
+                    />
+                    <br />
+
+                    <input
+                        type="number"
+                        className="form-control"
+                        required
+                        placeholder="Enter product cost"
+                        value={product_cost}
+                        onChange={(e) => {
+                            setProductCost(e.target.value);
+                        }}
+                    />
+                    <br />
+
+                    <select
+                        required
+                        onChange={(e) => setProductCategory(e.target.value)}
+                        className="form-select"
+                    >
+                        <option value="">Select category</option>
+                        <option value="televisions">TVs</option>
+                        <option value="phones">Phones</option>
+                        <option value="laptops">Laptops</option>
+                        <option value="accessories">Accessories</option>
+                    </select>
+                    <br />
+
+                    <textarea
+                        required
+                        value={product_description}
+                        onChange={(e) => setProductDescription(e.target.value)}
+                        rows="7"
+                        className="form-control"
+                    ></textarea>
+                    <br />
+
+                    <label htmlFor="" className="form-label">
+                        Product Image
+                    </label>
+                    <input
+                        type="file"
+                        accept="image/*"
+                        onChange={(e) => {
+                            setProductImage(e.target.files[0]);
+                        }}
+                        required
+                        className="form-control"
+                    />
+                    <br />
+
+                    <button className="btn btn-dark">Add product</button>
                 </form>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default AddProductComponent;
